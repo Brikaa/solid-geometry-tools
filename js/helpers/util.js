2c8d2a02d877cf3ab8117218d85a3e046be83588
Omar Brikaa <brikaaomar@gmail.com>
2021-12-29 22:15:46 +0200

    prototype

--- js/helpers/util.js
+++ js/helpers/util.js
@@ -1,14 +1,15 @@
 class Util {
-    static change_elements_visibility(element_ids, visibilities) {
-        if (typeof element_ids == 'string' && typeof visibilities == "string") {
-            document.getElementById(element_ids).style.display = visibilities;
-            return;
+    static change_elements_visibility(ids_to_visibility_map) {
+        for (const element_id in ids_to_visibility_map) {
+            const element = document.getElementById(element_id);
+            const visibility = ids_to_visibility_map[element_id] ? 'initial' : 'none';
+            element.style.display = visibility;
         }
-        if (element_ids.length !== visibilities.length) {
-            throw new Error('The length of element ids must match the length of the visibilities.');
-        }
-        for (let i = 0; i < element_ids.length; ++i) {
-            document.getElementById(element_ids[i]).style.display = visibilities[i];
-        }
+    }
+    static add_paragraph(text, parent_element_id) {
+        const parent_element = document.getElementById(parent_element_id);
+        const paragraph = document.createElement('p');
+        paragraph.innerHTML = text;
+        parent_element.appendChild(paragraph);
     }
 }
--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
 (function () {
+    const vectors = [];
+    const planes = [];
     document
         .getElementById('vector-button')
         .addEventListener('click', () =>
-            Util.change_elements_visibility(['vector-input', 'plane-input'], ['initial', 'none'])
+            Util.change_elements_visibility({ 'vector-input': true, 'plane-input': false })
         );
     document
         .getElementById('plane-button')
         .addEventListener('click', () =>
-            Util.change_elements_visibility(['plane-input', 'vector-input'], ['initial', 'none'])
+            Util.change_elements_visibility({ 'plane-input': true, 'vector-input': false })
         );
 })();
